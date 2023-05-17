616184eb13e5800ba61b4d35ba63febc6385d1f8
ConorCallanan2003 <conorcallanan03@gmail.com>
2023-05-17 18:10:28 +0100

    can handle multiple clients on multiple threads

--- EchoServer.java
+++ EchoServer.java
@@ -1,25 +1,61 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.PrimitiveIterator;
 
 
 public class EchoServer {
     public static void main(String[] args) {
 
+        List<ClientHandler> handlers = new ArrayList<ClientHandler>();
+
         try {
             System.out.println("Waiting and WAITING for clients....... 🥱🥱🥱🥱");
             ServerSocket serverSock = new ServerSocket(9806);
-            Socket soc = serverSock.accept(); // will return a socket object as soon as it gets a connection
-            System.out.println("Connection established babeeeyeyy 🐏🐏🐏🐏🎉🎉");
-            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
-            String str = in.readLine();
-            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
-            out.println("Server says: " + str);
+
+            while(true) {
+                Socket newSoc = serverSock.accept();
+                ClientHandler newHandler = new ClientHandler(newSoc);
+                newHandler.start();
+                handlers.add(newHandler);
+            }
+            // ClientHandler handler = new ClientHandler(soc);
+            // handler.start();
+
+            // handler.join();
+            // serverSock.close();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    
+}
+
+class ClientHandler extends Thread {
+
+    private Socket soc;
+
+    public ClientHandler(Socket soc) {
+        this.soc = soc;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Connection established babeeeyeyy 🐏🐏🐏🐏🎉🎉\nPort no. " + String.valueOf(soc.getPort()));
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()))) {
+            String str = in.readLine();
+            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+            out.println("Server says: " + str);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
